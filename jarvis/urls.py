fbcf4bf17991f5c4f64976afcd553cabd750aad9
abazante <alice.bazante@gmail.com>
2017-10-13 13:49:46 +0200

    Mise en place template de base + page accueil

--- jarvis/urls.py
+++ jarvis/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
+from jarvis import views
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$',  views.home, name="home"),
     url(r'^produits/', include('produit.urls')),
 ]
