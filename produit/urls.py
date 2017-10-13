e086ad27a04f6dacb09bb48771d77379238e67ec
abazante <alice.bazante@gmail.com>
2017-10-13 16:02:27 +0200

    Pagination produit

--- produit/urls.py
+++ produit/urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='indexProduit'),
-    url(r'^(?P<id>[0-9]+)/$', views.detail, name='detailProduit'),
+    url(r'^$', views.indexProduit, name='indexProduit'),
+    url(r'^(?P<id>[0-9]+)/$', views.detailProduit, name='detailProduit'),
 ]
