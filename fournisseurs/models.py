62c01a299a5fb54896d28d866b12479e1ccbd4ac
abazante <alice.bazante@gmail.com>
2017-11-14 22:48:14 +0100

    correction fournisseur

--- fournisseurs/models.py
+++ fournisseurs/models.py
@@ -7,8 +7,11 @@ from django.db import models
 
 class Fournisseur(models.Model):
    nom = models.CharField(max_length=30)
+   # Localisation
    adresse1 = models.CharField(max_length=100)
-   adresse2 = models.CharField(max_length=100)
+   adresse2 = models.CharField(max_length=100, blank=True)
+   codePostal = models.CharField(max_length=5, default="49000")
+   ville = models.CharField(max_length=80, default="Angers")
 
    def __str__(self):
        return self.nom
