3dd96b96060d9d57b4dd35b33811c8cb483515f9
abazante <alice.bazante@gmail.com>
2017-11-14 17:37:26 +0100

    Modification du panier

--- panier/urls.py
+++ panier/urls.py
@@ -5,4 +5,6 @@ from panier import views
 
 urlpatterns = [
     url(r'^$', views.panier, name="panier"),
+    url(r'^delete/(?P<id>[0-9]+)$', views.panierSup, name="panierSup"),
+    url(r'^modif/(?P<id>[0-9]+)$', views.panierModif, name="panierModif")
 ]
--- panier/views.py
+++ panier/views.py
@@ -1,19 +1,37 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render, redirect
 from cart.cart import Cart
 from produit.models import FicheProduit
+from produit.forms import *
 
 def add_to_cart(request, product_id, quantity):
     product = FicheProduit.objects.get(id=product_id)
     cart = Cart(request)
     cart.add(product, product.prix, quantity)
 
-def remove_from_cart(request, product_id):
+def update_from_cart(request, product_id, quantity):
+    product = FicheProduit.objects.get(id=product_id)
+    cart = Cart(request)
+    cart.update(product, quantity, product.prix)
+
+def delete_from_cart(request, product_id):
     product = FicheProduit.objects.get(id=product_id)
     cart = Cart(request)
     cart.remove(product)
 
 def panier(request):
-    return render_to_response('panier.html', dict(cart=Cart(request)))
+    form = ProduitForm()
+    return render(request,'panier.html', {'cart':Cart(request),'form':form})
+
+def panierSup(request, id):
+    delete_from_cart(request, id)
+    return redirect('panier') 
+
+def panierModif(request, id):
+    form = ProduitForm(request.POST)
+    if form.is_valid():
+        qte = form.cleaned_data['quantite']
+        update_from_cart(request, id, qte)
+    return redirect('panier') 
